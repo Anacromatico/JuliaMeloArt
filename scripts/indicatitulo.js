0514309ef0554e1792c7e4f2e3ada4baedae2453
Igor Gonçalves Bernardo <Igorg.bernardo@gmail.com>
2025-01-12 23:34:33 -0300

    v0.9.3

--- scripts/indicatitulo.js
+++ scripts/indicatitulo.js
@@ -1,4 +1,6 @@
 const h3Retratos = document.getElementById('retratos_titulo');
+const h3Paisagens = document.getElementById('paisagens_titulo');
+const h3Diversos = document.getElementById('diversos_titulo')
 
 function indicaTitulos (h3, titulo) {
     h3.innerHTML = titulo ;
